534679555d57856b31f7b0b39c6b845f45ac3422
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2021-11-01 16:38:58 +0500

    master

---
+++ src/Components/Product.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from'./product.css';
+
+const Product = () => {
+    return (  
+     <div>
+       <h3>Hellow</h3>
+       <h3>Hellow</h3>
+       <h3>Hellow</h3>
+
+
+     </div>
+
+
+    );
+}
+ 
+export default Product;
+
+
